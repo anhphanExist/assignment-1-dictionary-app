31bf63959b9996c00ce48eb675f346929a2a6f84
Anh Phan <37875446+anhphanExist@users.noreply.github.com>
2018-09-25 00:27:13 +0700

    Update Main.java

--- Ver1/Main.java
+++ Ver1/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     
     public static void main(String[] args) {
-        
-        Dictionary testDict = new Dictionary();
         DictionaryManagement.insertFromCommandline();
         DictionaryCommandline.showAllWords();
     }
