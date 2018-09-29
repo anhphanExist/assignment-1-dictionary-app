42932dac79052b94725b76482248205e6e0362c6
Anh Phan <37875446+anhphanExist@users.noreply.github.com>
2018-09-29 23:00:46 +0700

    Refactor and test dictionaryLookUp

--- Ver2/DictionaryCommandline.java
+++ Ver2/DictionaryCommandline.java
@@ -6,6 +6,16 @@ public class DictionaryCommandline {
      */
     public static void dictionaryBasic(){
         Dictionary basic = DictionaryManagement.insertFromCommandline();
+        showAllWords(basic);
+    }
+    
+    /**
+     * Read from file and print screen
+     */
+    public static void dictionaryAdvanced(){
+        Dictionary dict = DictionaryManagement.insertFromFile("src/data.txt");
+        showAllWords(dict);
+        DictionaryManagement.dictionaryLookup(dict);
     }
 
     /**
@@ -24,31 +34,17 @@ public class DictionaryCommandline {
     /**
      * export dictionary to file
      */
-    public static void exportToFile(String filename,Dictionary dict){
-        try (FileWriter out = new FileWriter(filename)) {
-            out.write("No  | English        | Vietnamese");
-            out.write(System.lineSeparator());
+    public static void exportToFile(String filename, Dictionary dict) {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(filename))) {
             int no = 0;
             for (Word curWord: dict.getDict()) {
                 no++;
-                out.write(String.format("%-4d| %-15s| " + curWord.getExplain(), no, curWord.getTarget()));
-                out.write(System.lineSeparator());
+                out.write(curWord.getTarget() + "\t" + curWord.getExplain());
+                out.newLine();
             }
         }
         catch (Exception e) {
-            System.out.println("Error: " + e.getClass().getSimpleName() + " - " + e.getMessage());
-
-            for (Throwable t: e.getSuppressed()) {
-                System.out.println("Surpress: " + t.getMessage());
-            }
-
+            DictionaryManagement.catchingException(e);
         }
     }
-
-    public static void dictionaryAdvanced(){
-        Dictionary dict = DictionaryManagement.insertFromFile("src/com/company/data.txt");
-        showAllWords(dict);
-        DictionaryManagement.dictionaryLookup(dict);
-    }
-
 }
--- Ver2/Main.java
+++ Ver2/Main.java
@@ -2,6 +2,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        DictionaryCommandline.dictionaryBasic();
+//        DictionaryCommandline.dictionaryBasic();
+        DictionaryCommandline.dictionaryAdvanced();
     }
 }
