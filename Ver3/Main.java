d708a5ec4d9507219747653f8e57ef15ff395a2a
Anh Phan <37875446+anhphanExist@users.noreply.github.com>
2018-10-05 17:22:14 +0700

    move catchingException method to Main

--- Ver3/Main.java
+++ Ver3/Main.java
@@ -1,8 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-
-//        DictionaryCommandline.dictionaryBasic();
-        DictionaryCommandline.dictionaryAdvanced();
+    
+        try (
+                // Initialize input stream
+                BufferedReader buff = new BufferedReader(new InputStreamReader(System.in))) {
+//            DictionaryCommandline.dictionaryBasic(buff);
+            DictionaryCommandline.dictionaryAdvanced(buff);
+        } catch (Exception e) {
+            catchingException(e);
+        }
     }
+    
+    //region Catching exception
+    /**
+     * print the catching error messeage
+     */
+    public static void catchingException(Exception e){
+        System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        for (Throwable t: e.getSuppressed()) {
+            System.out.println("Surpress: " + t.getMessage());
+        }
+    }
+    //endregion
 }
